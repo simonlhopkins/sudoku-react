c89cdf2dc06f978b4b3fc10f31923e26e37353ec
simonlhopkins <simonlhopkins@gmail.com>
2024-11-04 17:45:54 -0800

    fixed scuffed formatting

--- src/components/SudokuSection.tsx
+++ src/components/SudokuSection.tsx
@@ -1,71 +1,88 @@
 import styled from "styled-components";
 import SudokuSquare from "./SudokuSquare";
-import { SudokuSectionCoord, getIndexiesInSection, getSudokuBoardCoordFromIndex, getSudokuSectionCoordFromIndex } from "../util/Util";
+import {
+  SudokuSectionCoord,
+  getIndexiesInSection,
+  getSudokuBoardCoordFromIndex,
+  getSudokuSectionCoordFromIndex,
+} from "../util/Util";
 import { NotesMap } from "../util/gamestate";
 
-
 interface SudokuSectionProps {
-
-    sectionCoord: SudokuSectionCoord
-    populatedBoard: (number | null)[],
-    solvedBoard: (number | null)[],
-    startingBoard: (number | null)[];
-    onSquareClick: (index:number) => void,
-    focussedIndex: number
-    notes: NotesMap
+  sectionCoord: SudokuSectionCoord;
+  populatedBoard: (number | null)[];
+  solvedBoard: (number | null)[];
+  startingBoard: (number | null)[];
+  onSquareClick: (index: number) => void;
+  focussedIndex: number;
+  notes: NotesMap;
 }
 
+export default function SudokuSection({
+  sectionCoord,
+  startingBoard,
+  populatedBoard,
+  solvedBoard,
+  onSquareClick,
+  focussedIndex,
+  notes,
+}: SudokuSectionProps) {
+  function getIsHighlighted(index: number, focussedIndex: number) {
+    const assocSection = getSudokuSectionCoordFromIndex(index);
+    const focussedSection = getSudokuSectionCoordFromIndex(focussedIndex);
+    const assocBoardCoord = getSudokuBoardCoordFromIndex(index);
+    const focussedBoardCoord = getSudokuBoardCoordFromIndex(focussedIndex);
+    const sameRowOrCol =
+      assocBoardCoord.row == focussedBoardCoord.row ||
+      assocBoardCoord.col == focussedBoardCoord.col;
+    const sameSection =
+      assocSection.row == focussedSection.row &&
+      assocSection.col == focussedSection.col;
+    return sameSection || sameRowOrCol;
+  }
 
+  let sectionIndecies = getIndexiesInSection(sectionCoord);
+  let sectionSolved =
+    sectionIndecies.filter((index) => {
+      return populatedBoard[index] != solvedBoard[index];
+    }).length == 0;
 
-export default function SudokuSection({sectionCoord, startingBoard, populatedBoard, solvedBoard, onSquareClick, focussedIndex, notes }: SudokuSectionProps) {
-
-    
-    
-    function getIsHighlighted(index:number, focussedIndex:number){
-        const assocSection = getSudokuSectionCoordFromIndex(index);
-        const focussedSection = getSudokuSectionCoordFromIndex(focussedIndex);
-        const assocBoardCoord = getSudokuBoardCoordFromIndex(index);
-        const focussedBoardCoord = getSudokuBoardCoordFromIndex(focussedIndex);
-        const sameRowOrCol = assocBoardCoord.row == focussedBoardCoord.row || assocBoardCoord.col == focussedBoardCoord.col;
-        const sameSection = (assocSection.row == focussedSection.row && assocSection.col == focussedSection.col);
-        return sameSection || sameRowOrCol;
-    }
-
-    
-    let sectionIndecies = getIndexiesInSection(sectionCoord);
-    let sectionSolved = sectionIndecies.filter((index)=>{ return populatedBoard[index] != solvedBoard[index] }).length==0;
-    
-    let squares = sectionIndecies.map((index:number)=>{
-        const numberToRender = populatedBoard[index];
-        const actuallyValidPos = solvedBoard[index] == numberToRender;
-        return (<SudokuSquare
+  let squares = sectionIndecies.map((index: number) => {
+    const numberToRender = populatedBoard[index];
+    const actuallyValidPos = solvedBoard[index] == numberToRender;
+    return (
+      <SudokuSquare
         key={index}
         number={numberToRender}
-        index = {index}
-        correctPosition = {actuallyValidPos}
-        letterHighlighted = {(numberToRender == populatedBoard[focussedIndex])}
+        index={index}
+        correctPosition={actuallyValidPos}
+        letterHighlighted={numberToRender == populatedBoard[focussedIndex]}
         focussed={focussedIndex == index}
         backgroundHighlighted={getIsHighlighted(index, focussedIndex)}
         immutable={startingBoard[index] != null}
         onClicked={onSquareClick}
-        notes= {notes.get(index)}
-        />);
-    });
+        notes={notes.get(index)}
+      />
+    );
+  });
 
-    return (
-        <StyledSudokuSection className={sectionSolved?"solved":""}>
-            {squares}
-        </StyledSudokuSection>
-    )
+  return (
+    <StyledSudokuSection className={sectionSolved ? "solved" : ""}>
+      {squares}
+    </StyledSudokuSection>
+  );
 }
 
 const StyledSudokuSection = styled.div`
-    display: grid;
-    border: 1px solid ${props => props.theme.colors.BEIGE.DARK};
-    box-sizing: border-box;
-    grid-template-columns: repeat(3, 1fr);
-    transition: border 0.2s ease;
-    &.solved{
-        border:4px solid ${props => props.theme.colors.GREEN};
-    }
-`
+  display: grid;
+  border: 1px solid ${(props) => props.theme.colors.BEIGE.DARK};
+  box-sizing: border-box;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  width: 100%;
+  height: 100%;
+  transition: border 0.2s ease;
+  &.solved {
+    border: 4px solid ${(props) => props.theme.colors.GREEN};
+  }
+`;
--- src/components/SudokuSquare.tsx
+++ src/components/SudokuSquare.tsx
@@ -1,113 +1,124 @@
-import styled from "styled-components"
-interface SudokuSquareProps{
-    number: number | null,
-    immutable: boolean,
-    backgroundHighlighted: boolean,
-    letterHighlighted: boolean,
-    correctPosition: boolean,
-    onClicked: (index:number, immutable:boolean)=>void,
-    index: number,
-    focussed: boolean,
-    notes: undefined | Set<number>
+import styled from "styled-components";
+interface SudokuSquareProps {
+  number: number | null;
+  immutable: boolean;
+  backgroundHighlighted: boolean;
+  letterHighlighted: boolean;
+  correctPosition: boolean;
+  onClicked: (index: number, immutable: boolean) => void;
+  index: number;
+  focussed: boolean;
+  notes: undefined | Set<number>;
 }
 
-export default function SudokuSquare({number, index, onClicked, immutable, correctPosition, focussed, backgroundHighlighted, letterHighlighted, notes}:SudokuSquareProps){
-
-    const incorrectPosition = !correctPosition && number!=null;
-    const classes = [
-        (focussed&&!immutable)&&"focussed",
-        immutable&&"immutable",
-        incorrectPosition&&"incorrectPosition",
-        backgroundHighlighted&&"backgroundHighlighted",
-        letterHighlighted&&"letterHighlighted"
-    ]
+export default function SudokuSquare({
+  number,
+  index,
+  onClicked,
+  immutable,
+  correctPosition,
+  focussed,
+  backgroundHighlighted,
+  letterHighlighted,
+  notes,
+}: SudokuSquareProps) {
+  const incorrectPosition = !correctPosition && number != null;
+  const classes = [
+    focussed && !immutable && "focussed",
+    immutable && "immutable",
+    incorrectPosition && "incorrectPosition",
+    backgroundHighlighted && "backgroundHighlighted",
+    letterHighlighted && "letterHighlighted",
+  ]
     .filter(Boolean)
     .join(" ");
 
-    
-
-    return(
-        <SudokuSquareParent
-
-            className={classes}
-            onClick={()=>{
-                onClicked(index, immutable)
-            }}
-        >
-            {number==null&&<SquareNotes>
-                {[...Array(9).keys()].map(i=><div key ={i}><p>{notes&&notes.has(i)?i+1:""}</p></div>)}
-            </SquareNotes>}
-            <span>{number==null?"":number+1}</span>
-        </SudokuSquareParent>
-    )
+  return (
+    <SudokuSquareParent
+      className={classes}
+      onClick={() => {
+        onClicked(index, immutable);
+      }}
+    >
+      {number == null && (
+        <SquareNotes>
+          {[...Array(9).keys()].map((i) => (
+            <div key={i}>
+              <p>{notes && notes.has(i) ? i + 1 : ""}</p>
+            </div>
+          ))}
+        </SquareNotes>
+      )}
+      <span>{number == null ? "" : number + 1}</span>
+    </SudokuSquareParent>
+  );
 }
 
 const SquareNotes = styled.div`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(3, 1fr);
-    gap: 5%;
-    >div{
-        @media only screen and (min-width: 500px){
-            font-size: 16px;
-        }
-        font-size: 12px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        box-sizing: border-box;
-        overflow: hidden;
-        color: ${props => props.theme.colors.PINK};
-        p{
-            margin: 0;
-        }
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 5%;
+  > div {
+    @media only screen and (min-width: 500px) {
+      font-size: 16px;
     }
-`
+    font-size: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    overflow: hidden;
+    color: ${(props) => props.theme.colors.PINK};
+    p {
+      margin: 0;
+    }
+  }
+`;
 
 const SudokuSquareParent = styled.div`
-    position: relative;
-    border: 1px solid ${props => props.theme.colors.BEIGE.DARK};
-    background-color: ${props => props.theme.colors.BEIGE.BASE};
-    display: flex;
-    box-sizing: border-box;
-    align-items: center;
-    aspect-ratio: 1;
-    justify-content: center;
-    font-size: 1rem;
-    -webkit-user-select: none;
-    user-select:none;
-    transition-property: color, background-color, border;
-    transition-duration: 0.2s;
-    transition-timing-function: ease;
-    
-    span{
-        z-index: 1;
-    }
-    @media only screen and (min-width: 500px){
-        font-size: 2rem;
-    }
-    &.immutable{
-        background-color: ${props => props.theme.colors.BEIGE.MEDIUM};
-    }
-    &.backgroundHighlighted{
-        background-color: ${props => props.theme.colors.YELLOW.BASE};
-        &.immutable{
-            background-color: ${props => props.theme.colors.YELLOW.DARK};
-        }
-        
-    }
-    &.focussed {
-        border: 3px solid ${props => props.theme.colors.GREEN};
-        background-color: ${props => props.theme.colors.BEIGE.DARK};
-    }
+  position: relative;
+  border: 1px solid ${(props) => props.theme.colors.BEIGE.DARK};
+  background-color: ${(props) => props.theme.colors.BEIGE.BASE};
+  display: flex;
+  box-sizing: border-box;
+  align-items: center;
+  aspect-ratio: 1;
+  justify-content: center;
+  font-size: 1rem;
+  -webkit-user-select: none;
+  user-select: none;
+  transition-property: color, background-color, border;
+  transition-duration: 0.2s;
+  transition-timing-function: ease;
 
-    &.incorrectPosition{
-        background-color: ${props => props.theme.colors.RED};
+  span {
+    z-index: 1;
+  }
+  @media only screen and (min-width: 500px) {
+    font-size: 2rem;
+  }
+  &.immutable {
+    background-color: ${(props) => props.theme.colors.BEIGE.MEDIUM};
+  }
+  &.backgroundHighlighted {
+    background-color: ${(props) => props.theme.colors.YELLOW.BASE};
+    &.immutable {
+      background-color: ${(props) => props.theme.colors.YELLOW.DARK};
     }
-    &.letterHighlighted{
-        color: ${props => props.theme.colors.GREEN};
-    }
-`
+  }
+  &.focussed {
+    border: 3px solid ${(props) => props.theme.colors.GREEN};
+    background-color: ${(props) => props.theme.colors.BEIGE.DARK};
+  }
+
+  &.incorrectPosition {
+    background-color: ${(props) => props.theme.colors.RED};
+  }
+  &.letterHighlighted {
+    color: ${(props) => props.theme.colors.GREEN};
+  }
+`;
